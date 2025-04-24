8cbcec295085636625dc28f1ea945e4800a9a5fa
AntonioLlanes <159671172+AntonioLlanes@users.noreply.github.com>
2025-04-24 03:55:26 -0400

    all the project.

--- Vintage/settings.py
+++ Vintage/settings.py
@@ -121,11 +121,14 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
